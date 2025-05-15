f65da119b835dd450f7af3713ed77cc6b471ecce
Edgar Saenz <edgarsaenz243@gmail.com>
2025-05-15 02:27:12 -0600

    delete buttons

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"cf56914b326edb0ccb123ffdc60f00060bd513
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "32901715"
+    serviceWorkerVersion: "3658209609"
   }
 });
